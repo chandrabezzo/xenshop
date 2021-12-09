32f8276191aac8d106db706a6f11e212c41998d6
Chandra Abdul Fattah <chandrashibezzo@gmail.com>
2021-12-09 16:40:07 +0700

    Integration Product Page

--- lib/features/splash/presentation/get/splash_controller.dart
+++ lib/features/splash/presentation/get/splash_controller.dart
@@ -1,11 +1,11 @@
 import 'package:get/get.dart';
-import '../../../main/presentation/pages/main_page.dart';
+import 'package:xenshop_product/xenshop_product.dart';
 
 class SplashController extends GetxController {
   @override
   void onInit() async {
     super.onInit();
     await Future.delayed(const Duration(seconds: 3));
-    Get.offAllNamed(MainPage.routeName);
+    Get.offAllNamed(BaseProductPage.routeName);
   }
 }
